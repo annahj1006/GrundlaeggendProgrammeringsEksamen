6fdeabb51f32995ef439c12bbf6dc0356e7d8308
alwo78 <61276696+alwo78@users.noreply.github.com>
2022-11-24 14:10:17 +0100

    redigeret i datafil

--- src/main/java/Data/MovieData.java
+++ src/main/java/Data/MovieData.java
@@ -5,7 +5,7 @@ import java.io.File;
 
 public class MovieData {
     public static void main(String[] args) throws Exception {
-        File file = new File("././resources/Data/film.txt");
+        File file = new File("C:\\Users\\Alice\\Documents\\GitHub\\GrundlaeggendProgrammeringsEksamen\\src\\main\\resources\\Data\film.txt");
         Scanner s = new Scanner(file);
         System.out.println(s.nextLine());
     }
