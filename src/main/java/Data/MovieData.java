43b48f91e998bb669c048b886a20b3783e162662
alwo78 <61276696+alwo78@users.noreply.github.com>
2022-11-24 14:40:24 +0100

    Redigeret i datafil

--- src/main/java/Data/MovieData.java
+++ src/main/java/Data/MovieData.java
@@ -3,11 +3,22 @@ package Data;
 import java.util.*;
 import java.io.File;
 
+
 public class MovieData {
+
     public static void main(String[] args) throws Exception {
+        HashMap film;
+        HashMap serie;
         File file = new File("C:\\Users\\Alice\\Documents\\GitHub\\GrundlaeggendProgrammeringsEksamen\\src\\main\\resources\\Data\\film.txt");
         Scanner s = new Scanner(file);
-        System.out.println(s.nextLine());
+        //System.out.println(s.nextLine());
+        film = new HashMap<String,ArrayList>();
+        serie = new HashMap<String,ArrayList>();
+        while(s.hasNextLine()){
+            String[] line = s.nextLine().split("; ");
+            film.put(line[0], "value");
+
+        }
     }
 
 
