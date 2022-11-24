0f927fe6ea342f48d764d51f98fc4bc69a38bead
alwo78 <61276696+alwo78@users.noreply.github.com>
2022-11-24 14:06:59 +0100

    redigeret i datafil

--- src/main/java/Data/MovieData.java
+++ src/main/java/Data/MovieData.java
@@ -1,4 +1,16 @@
 package Data;
 
+import java.util.*;
+import java.io.File;
+
 public class MovieData {
+    public static void main(String[] args) throws Exception {
+        File file = new File("././resources/Data/film.txt");
+        Scanner s = new Scanner(file);
+        System.out.println(s.nextLine());
+    }
+
+
+
+
 }
