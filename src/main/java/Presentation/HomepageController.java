934fa2412897370b8976a4a256210bf97fb2ea21
Anna <annahj1006@gmail.com>
2022-11-29 10:32:21 +0100

    HompageController initialize

--- src/main/java/Presentation/HomepageController.java
+++ src/main/java/Presentation/HomepageController.java
@@ -1,4 +1,64 @@
 package Presentation;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import org.w3c.dom.Text;
+
+import java.awt.*;
+
 public class HomepageController {
+    @FXML
+    Button FilmButton;
+
+    @FXML
+    Button SerierButton;
+
+    @FXML
+    Button GenreButton;
+
+    @FXML
+    Button MinListeButton;
+
+    @FXML
+    Button AccountButton;
+
+    @FXML
+    Button HomeButton;
+
+    @FXML
+    TextField SearchBar;
+
+    @FXML
+    GridPane MediaGrid;
+
+    @FXML
+    public void HomeButtonPressed(){
+
+    }
+
+    @FXML
+    public void FilmButtonPressed(){
+
+    }
+
+    @FXML
+    public void SerierButtonPressed(){
+
+    }
+
+    @FXML
+    public void GenreButtonPressed(){
+
+    }
+
+    @FXML
+    public void MinListeButtonPressed(){
+
+    }
+
+    @FXML
+    public void AccountButtonPressed(){
+
+    }
+
 }
