f526b830d0da12916b900dcae0e8c1343ad43754
Anna <annahj1006@gmail.com>
2022-11-29 10:41:09 +0100

    Grid new script

---
+++ src/main/java/Domain/Grid.java
@@ -0,0 +1,18 @@
+package Domain;
+
+import Presentation.HomepageController;
+import javafx.scene.layout.GridPane;
+
+public class Grid {
+    private GridPane MediaGrid;
+    private HomepageController homepageController;
+
+    public Grid(GridPane MediaGrid,HomepageController homepageController){
+        this.MediaGrid = MediaGrid;
+        this.homepageController = homepageController;
+    }
+
+    public void MediaInGrid(){
+
+    }
+}
