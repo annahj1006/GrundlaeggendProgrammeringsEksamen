f2b80a35b2261f861299e5857ea454e1f9df19ca
Anna <annahj1006@gmail.com>
2022-12-06 13:30:32 +0100

    Playpage update

--- src/main/java/Presentation/MediaController.java
+++ src/main/java/Presentation/MediaController.java
@@ -1,11 +1,20 @@
 package Presentation;
 
+import Domain.Grid;
 import Domain.Media;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MediaController {
 
@@ -21,8 +30,22 @@ public class MediaController {
     @FXML
     private Button playButton;
 
-    public void setMedia(Media media){
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void setMedia(Media media) {
         mediaImage.setImage(new Image("file:src/main/resources/Data/filmplakater/" + media.getPoster() + ".jpg"));
+        mediaTitle.setText(media.getName());
     }
 
+    @FXML
+    public void playButtonPressed(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(App.class.getResource("fxml/Playpage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/main/java/Presentation/PlaypageController.java
+++ src/main/java/Presentation/PlaypageController.java
@@ -1,7 +1,46 @@
 package Presentation;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PlaypageController {
+    @FXML
+    private Button AccountButton1;
+
+    @FXML
+    private Button HomeButton1;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void homeButtonPressed(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(App.class.getResource("/fxml/HomePage.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    public void accountButtonPressedHomePage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(App.class.getResource("/fxml/LoginPage.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 
 }
