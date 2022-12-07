83052bb27751f4497cd9f9379d186861e5ac550d
OhagITU <ohag@itu.dk>
2022-12-07 21:57:04 +0100

    x

--- src/main/java/Presentation/App.java
+++ src/main/java/Presentation/App.java
@@ -9,27 +9,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 
-public class App /*extends Application*/ {
+public class App extends Application {
 
 
 
     public static void main(String[] args){
-        //launch();
+        launch();
 
-        Operations x = new Operations();
-        for(Media m : x.mix) {
-            System.out.println(m.toString());
-        }
+
 
     }
 
-    /*@Override
+    @Override
     public void start(Stage startingStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         startingStage.setTitle("L");
         startingStage.setScene(scene);
         startingStage.show();
-    }*/
+    }
 
 }
