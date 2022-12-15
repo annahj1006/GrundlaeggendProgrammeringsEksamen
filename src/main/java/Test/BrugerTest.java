3dc5a3966ff7ca7c0c833f0f5364d3819eceb146
OhagITU <ohag@itu.dk>
2022-12-15 12:33:34 +0100

    Lavet junit tests til Operation/Instatiate og Bruger

--- src/main/java/Test/BrugerTest.java
+++ src/main/java/Test/BrugerTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 
 import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
 
 
 import static org.junit.Assert.*;
@@ -38,23 +40,27 @@ public class BrugerTest {
     // Checking if the users favorite list is correct after adding elements.
     @Test
     public void addToFavoriteListe() {
+        List<Media> expectedResult = o.search("1");
+
         Media toAdd0 = new Movie("movie", "12 Angry Men", "1957", "Crime, Drama", "8,9", "src/main/resources/Data/filmplakater/12 Angry Men.JPG");
         Media toAdd1 = new Movie("movie", "2001 A Space Odyssey", "1968", "Adventure, Sci-fi", "8,3", "src/main/resources/Data/filmplakater/2001 A Space Odyssey.JPG");
         nyBruger.addMedia(toAdd0);
         nyBruger.addMedia(toAdd1);
-        assertEquals(o.search("1"), nyBruger.getMyList());
+        assertEquals(expectedResult, nyBruger.getMyList());
     }
 
     // Checkinf if the users favorite list is correct after removing one element
     @Test
     public void removeFromFavoriteLise() {
+        List<Media> expectedResult = o.search("2001 A Space Odyssey");
+
         Media toAdd0 = new Movie("movie", "12 Angry Men", "1957", "Crime, Drama", "8,9", "src/main/resources/Data/filmplakater/12 Angry Men.JPG");
         Media toAdd1 = new Movie("movie", "2001 A Space Odyssey", "1968", "Adventure, Sci-fi", "8,3", "src/main/resources/Data/filmplakater/2001 A Space Odyssey.JPG");
         nyBruger.addMedia(toAdd0);
         nyBruger.addMedia(toAdd1);
-
         nyBruger.removeMedia(toAdd0);
-        assertEquals(o.search("2001"), nyBruger.getMyList());
+
+        assertEquals(expectedResult, nyBruger.getMyList());
     }
 
     @Test
